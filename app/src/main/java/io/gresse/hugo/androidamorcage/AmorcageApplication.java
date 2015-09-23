cc6dacbfab0db902b98f00a72eb70606cd6f906c
Hugo Gresse <hugo.gresse@teads.tv>
2015-09-23 13:51:41 +0200

    initial base app

---
+++ app/src/main/java/io/gresse/hugo/androidamorcage/AmorcageApplication.java
@@ -0,0 +1,14 @@
+package io.gresse.hugo.androidamorcage;
+
+import android.app.Application;
+
+/**
+ * Main application that register app services
+ *
+ * Created by Hugo Gresse on 23/09/15.
+ */
+public class AmorcageApplication extends Application {
+
+
+
+}
